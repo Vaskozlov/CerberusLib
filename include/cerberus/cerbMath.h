40c74a6dec3872223d33189d6fedad3521e6567e
vaskozlov3940@gmail.com <Vasya28102004>
2021-06-16 21:22:46 +0500

    Libcerberus

---
+++ include/cerberus/cerbMath.h
@@ -0,0 +1,53 @@
+#include <float.h>
+#include <stddef.h>
+#include <inttypes.h>
+
+#if !defined(__cplusplus)
+
+    typedef union {
+        uint64_t value;
+        double floatingPoint;
+    } IEEE754u64_t; 
+
+    typedef union {
+        uint32_t value;
+        float floatingPoint;
+    } IEEE754u32_t; 
+
+    float cerbABSf(float x);
+    double cerbABS(double x);
+#else
+
+namespace cerb{
+
+    union IEEE754u32_t{
+        uint32_t value;
+        float floatingPoint;
+
+        constexpr IEEE754u32_t(uint32_t v) : value(v) {};
+        constexpr IEEE754u32_t(float f) : floatingPoint(f) {};
+    };
+
+    union IEEE754u64_t{
+        uint64_t value;
+        double floatingPoint;
+
+        constexpr IEEE754u64_t(uint32_t v) : value(v) {};
+        constexpr IEEE754u64_t(double f) : floatingPoint(f) {};
+    };
+
+    constexpr float ABS(float x){
+        IEEE754u32_t ieee754(x);
+        ieee754.floatingPoint = x;
+        ieee754.value &= (~((uint32_t)0) ^ ((uint32_t)1 << 31));
+        return ieee754.floatingPoint;
+    }
+
+    constexpr double ABS(double x){
+        IEEE754u64_t ieee754(x);
+        ieee754.value &= (~((uint64_t)0) ^ ((uint64_t)1 << 63));
+        return ieee754.floatingPoint;
+    }
+}
+
+#endif /* __cplusplus */
---
+++ include/cerberus/cerberus.h
@@ -0,0 +1,23 @@
+#ifndef cerberus_h
+#define cerberus_h
+
+#ifndef STRX
+#  define STRX(x) #x
+#  define STR(x) STRX(x)
+#endif /* STRX */
+
+#if defined(__clang__)
+#  define COMPILER_NAME "clang"
+#elif defined(__GNUC__)
+#  define COMPILER_NAME "gcc"
+#endif
+
+#include <cerberus/cerbMath.h>
+
+#if defined(__cplusplus)
+#  include <cerberus/cerberusc++.hpp>
+#else
+#  include <cerberus/cerberusc.h>
+#endif /* __cplusplus */
+
+#endif /* cerberus_h */
